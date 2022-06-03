67516fb0c0eb8a77de73bfb34f4d0dee151bf5e3
Polyakov <polyakovmymail@gmail.com>
2022-06-03 18:29:42 +0500

    дополнен интерфейс авторизации

--- DesktopClient/Login.Designer.cs
+++ DesktopClient/Login.Designer.cs
@@ -33,6 +33,10 @@
             this.textBoxServerAddress = new System.Windows.Forms.TextBox();
             this.textBoxServerPort = new System.Windows.Forms.TextBox();
             this.buttonConnection = new System.Windows.Forms.Button();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -71,7 +75,7 @@
             // 
             // buttonConnection
             // 
-            this.buttonConnection.Location = new System.Drawing.Point(82, 89);
+            this.buttonConnection.Location = new System.Drawing.Point(159, 89);
             this.buttonConnection.Name = "buttonConnection";
             this.buttonConnection.Size = new System.Drawing.Size(97, 23);
             this.buttonConnection.TabIndex = 4;
@@ -79,11 +83,47 @@
             this.buttonConnection.UseVisualStyleBackColor = true;
             this.buttonConnection.Click += new System.EventHandler(this.buttonConnection_Click);
             // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(277, 22);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(100, 23);
+            this.textBoxLogin.TabIndex = 5;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(277, 54);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPass.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(209, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Логин: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(209, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Пароль: ";
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(265, 145);
+            this.ClientSize = new System.Drawing.Size(423, 143);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxLogin);
             this.Controls.Add(this.buttonConnection);
             this.Controls.Add(this.textBoxServerPort);
             this.Controls.Add(this.textBoxServerAddress);
@@ -103,5 +143,9 @@
         private TextBox textBoxServerAddress;
         private TextBox textBoxServerPort;
         private Button buttonConnection;
+        private TextBox textBoxLogin;
+        private TextBox textBoxPass;
+        private Label label3;
+        private Label label4;
     }
 }
